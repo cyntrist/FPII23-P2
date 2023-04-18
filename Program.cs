814fc5245a4807355183dcd4621862106d91bc0b
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-04-18 22:50:33 +0200

    process command inicio

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// Cynthia Tristán Álvarez
 // Paula Sierra Luque
 
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using AdventureGame;
 using Listas;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdventureGame
 {
@@ -20,6 +22,11 @@ namespace AdventureGame
             ReadRooms(ROOMS_FILE, map);
             map.SetItemsRooms();
             map.WriteMap();
+
+            while (true) // bucle ppal.
+            {
+
+            }
         }
 
         #region Métodos Read
@@ -69,6 +76,41 @@ namespace AdventureGame
                 map.AddRouteRoom(n, bits[0], int.Parse(bits[1]), condItem); // Añade a la habitación en el mapa la ruta leída
                 newline = f.ReadLine()!;                                  // Siguiente línea
             }                                                        // Si la siguiente línea es blanca, acaba el método
+        }
+
+        void ProcessCommand(Map map, string input, int playerRoom, ListaEnlazada inventory)
+        {
+            string[] words = input.Trim().ToUpper().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            switch (words[0])
+            {
+                case "HELP": // muestra la ayuda del juego
+                    Console.WriteLine("AYUDA: "); // falta ponerla
+                    break;
+                case "INVENTORY": // muestra el inventario actual del jugador
+                    Console.WriteLine(inventory.ToString());
+                    break;
+                case "LOOK": // muestra la información de la habitación actual
+                    Console.WriteLine(map.GetInfoRoom(playerRoom));
+                    break;
+                case "ITEMS": // muestra los ítems de la habitación actual
+                    Console.WriteLine(map.GetItemsRoom(playerRoom));
+                    break;
+                case "TAKE " + "<item>": // si el item está en habitación actual lo recoge
+                                         // y lo añade al inventario del jugador;
+                                         // mensaje de error en otro caso
+                    // no se muy bien como hacer lo del string comando
+                    break;
+                case "DROP " + "<item>": // si el ítem está en el inventario del jugador,
+                                         // lo elimina del inventario y lo deja en la habitación actual;
+                                         // mensaje de error en otro caso
+                    // no se muy bien como hacer lo del string comando
+                    break;
+                default: // se interpreta como dirección de movimiento,
+                         // que se gestionará con el método correspondiente de Map.
+                    map.Move(playerRoom, words[0], inventory);
+                    break;
+            }
         }
         #endregion
     }
