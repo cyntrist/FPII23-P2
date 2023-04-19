54d7f040d59f60bac42a41090911523122486dde
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-04-19 22:34:33 +0200

    limpieza de main

--- Program.cs
+++ Program.cs
@@ -14,25 +14,23 @@ namespace AdventureGame
 
         static void Main()
         {
-            Map map                 = new();                            // MAPEADO
+            Map map                 = new();                              // INICIALIZACIÓN
             ListaEnlazada inventory = new();
-            //AutocompletadoComandos autocompletado = new(); //wip
             int playerRoom          = 1;
 
-            ReadInventory(ITEMS_FILE, map); 
+            ReadInventory(ITEMS_FILE, map);                               // LECTURA 
             ReadRooms(ROOMS_FILE, map);
 
-            map.SetItemsRooms();
+            map.SetItemsRooms();                                          // RELACIÓN
             //map.WriteMap();
 
-            Console.Write("Welcome to Adventure!! "); Instructions();   // INTRODUCCIÓN
-            string[] commands = ReadCommands();                         // HISTORIAL DE COMANDOS DE ARCHIVO
-            Console.WriteLine("\n" + map.GetInfoRoom(playerRoom) + "\n");      // INFORMACIÓN INICIAL
+            Console.Write("Welcome to Adventure!! "); Instructions();     // introducción
+            string[] commands = ReadCommands();                           // HISTORIAL DE COMANDOS DE ARCHIVO
+            Console.WriteLine("\n" + map.GetInfoRoom(playerRoom) + "\n"); // información sala inicial
 
-            string historial = string.Empty; // historial de comandos de esta sesión
-            if (commands != null && commands.Length > 0) 
-                // comprueba que existe el archivo y tiene por lo menos una línea
-            {
+            string historial = string.Empty;                              // historial de sesión
+            if (commands != null && commands.Length > 0)
+            {  // comprueba que existe el archivo y tiene por lo menos una línea
                 Console.WriteLine("Command History:\n");
                 for (int i = 0; i < commands.Length; i++) // para cada comando
                 {
@@ -41,31 +39,23 @@ namespace AdventureGame
                 }
             }
 
-            while (playerRoom > 0)                                      // BUCLE PRINCIPAL
+            while (playerRoom > 0)                                        // BUCLE PRINCIPAL
             {
                 Console.Write("> ");
                 string input = Console.ReadLine()!;
                 ProcessCommand(map, input, ref playerRoom, inventory);
                 if (input.Trim().ToUpper() != "END") 
                     historial += "," + input;
-
-                // EXTENSION
-                //ConsoleKeyInfo key = Console.ReadKey(true);
-                //if(key.Key == ConsoleKey.Tab) //si se pulsa tabulador
-                {
-                    //se buscan comandos para autocompletar
-                    //autocompletado.BuscarComandos(Console.ReadLine()!);
-                }
             }
 
-            if (SaveCommands(historial))                                                      // GUARDADO DE HISTORIAL
+            if (SaveCommands(historial))                                  // GUARDADO DE HISTORIAL
                 Console.WriteLine("The game has been saved succesfully.");
             else Console.WriteLine("The game has ended without saving.");
         }
 
         #region Métodos Read
         static void ReadInventory(string file, Map map)
-        //lee archivo file y escribe informacion en pantalla
+        // lee archivo file y escribe informacion en pantalla
         {
             StreamReader sr = null!;
             try
@@ -143,14 +133,15 @@ namespace AdventureGame
                                         + "\n\tItems: shows the items in the current room."
                                         + "\n\tTake <item>: moves the item in the room to your inventory."
                                         + "\n\tDrop <item>: moves the item in your inventory to the room."
-                                        + "\n\t<direction>: moves you towards the specified direction."); 
+                                        + "\n\t<direction>: moves you towards the specified direction."
+                                        + "\n\tEnd: tries to save and finishes the game."); 
                         break;
 
                     case "INVENTORY": // muestra el inventario actual del jugador
                         string inventario = map.GetItemsInfo(inventory);
                         if (inventario.Trim() == string.Empty) // si está vacío
                             Console.WriteLine("There are no items in your inventory.");
-                        else Console.WriteLine(inventario);
+                        else Console.WriteLine(inventario); // si no, lo muestra
                         break;
 
                     case "LOOK": // muestra la información de la habitación actual
@@ -161,17 +152,17 @@ namespace AdventureGame
                         string items = map.GetItemsRoom(playerRoom);
                         if (items.Trim() == string.Empty) // si está vacío
                             Console.WriteLine("There are no items in your inventory.");
-                        else Console.WriteLine(items);
+                        else Console.WriteLine(items); // si no, lo muestra;
                         break;
 
                     case "TAKE": // si el item está en habitación actual lo recoge
                                  // y lo añade al inventario del jugador;
                                  // mensaje de error en otro caso
                         if (words.Length > 1) // si hay una 2ª palabra
-                            if (map.TakeItemRoom(playerRoom, words[1], inventory))
-                                Console.WriteLine(words[1] + " taken.");
-                            else Console.WriteLine("There is no such item in the room.");
-                        else Console.WriteLine("Please specify an item to take.");
+                            if (map.TakeItemRoom(playerRoom, words[1], inventory)) 
+                                Console.WriteLine(words[1] + " taken."); // si sí es un ítem válido
+                            else Console.WriteLine("There is no such item in the room."); // si no es un ítem válido
+                        else Console.WriteLine("Please specify an item to take."); // si no hay 2ª palabra
                         break;
 
                     case "DROP": // si el ítem está en el inventario del jugador,
@@ -179,9 +170,9 @@ namespace AdventureGame
                                  // mensaje de error en otro caso
                         if (words.Length > 1) // si hay una 2ª palabra
                             if (map.DropItemRoom(playerRoom, words[1], inventory))
-                                Console.WriteLine(words[1] + " dropped.");
-                            else Console.WriteLine("There is no such item in your inventory.");
-                        else Console.WriteLine("Please specify an item to drop.");
+                                Console.WriteLine(words[1] + " dropped."); // si sí es un ítem válido
+                            else Console.WriteLine("There is no such item in your inventory."); // si no es un ítem válido
+                        else Console.WriteLine("Please specify an item to drop."); // si no hay 2ª palabra
                         break;
 
                     case "END": // sale del programa
@@ -207,7 +198,8 @@ namespace AdventureGame
         {
             Console.WriteLine("Would you like instructions?");
             Console.Write("\n> ");
-            if (Console.ReadLine()!.Trim().ToUpper() == "YES") 
+            string input = Console.ReadLine()!.Trim().ToUpper();
+            if (input == "YES" || input == "Y") 
                 Console.WriteLine("\nSomewhere nearby is Colossal Cave, where others have found fortunes " +
                     "in treasure and gold, though it is rumored that some who enter are never seen again. " +
                     " Magic is said to work in the cave.  I will be your eyes and hands.  Direct me with " +
@@ -215,7 +207,7 @@ namespace AdventureGame
         }
     
         static string[] ReadCommands()
-        { // lea comandos de un archivo dado (uno por línea, por ejemplo) y los ejecute en secuencia.
+        { // lee comandos de un archivo dado (uno por línea, por ejemplo) y los ejecuta en secuencia.
             StreamReader sr = null!;
             string[] comandos = null!;
             try
@@ -229,20 +221,20 @@ namespace AdventureGame
                     i++;
                 }
             }
-            catch (FileNotFoundException fnfe) { Console.WriteLine("There is no command save file."); }
+            catch (FileNotFoundException) { Console.WriteLine("There is no command save file."); }
             catch (Exception e) { Console.WriteLine($"Error: " + e.Message); }
             finally { sr?.Close(); }
             return comandos;
         }
 
-        static bool SaveCommands(string commands)
-        {
+        static bool SaveCommands(string history)
+        { // añade el historial de comandos nuevos (de la sesión de juego actual) al archivo de guardado anterior si existe (acumulativo)
             bool retorno = false;
             StreamWriter sw = null!;
             try
             {
-                sw = new StreamWriter(COMMANDS_FILE, true);
-                string[] words = commands.Trim().ToUpper().Split(",", StringSplitOptions.RemoveEmptyEntries);
+                sw = new StreamWriter(COMMANDS_FILE, true); // append = true
+                string[] words = history.Trim().ToUpper().Split(",", StringSplitOptions.RemoveEmptyEntries);
                 for (int i = 0; i < words.Length; i++)
                     sw.WriteLine(words[i]);
                 retorno = true;
